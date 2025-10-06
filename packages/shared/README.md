019daa3d059783ec9551c066c475957afc9a87e7
Andrew Dyer <andrewdyer@outlook.com>
2025-10-06 21:22:57 +0100

    feat: :sparkles: add @repo/shared package
    Add shared package with base components and styles.

---
+++ packages/shared/README.md
@@ -0,0 +1,13 @@
+# @repo/shared
+
+A shared package for reusable code within the monorepo.
+
+## Installation
+
+To install `@repo/shared` in a specific app within the monorepo, run:
+
+```bash
+pnpm add @repo/shared --filter <app-package>
+```
+
+Replace `<app-package>` with the name or path of the app that will use this package.
---
+++ packages/shared/vite.config.ts
@@ -0,0 +1,37 @@
+import path from 'path';
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+      outDir: 'dist/types',
+      entryRoot: 'src',
+    }),
+  ],
+  build: {
+    cssCodeSplit: true,
+    lib: {
+      entry: {
+        index: path.resolve(__dirname, 'src/index.ts'),
+        components: path.resolve(__dirname, 'src/components/index.ts'),
+      },
+      name: 'shared',
+      formats: ['es'],
+      fileName: (_, entryName) => `${entryName}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
+});
