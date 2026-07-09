d0fb0165fdbda4eea542bddcd3509219bbf22912
Andrew Dyer <andrewdyer@outlook.com>
2026-07-09 15:19:08 +0100

    chore: add commitlint configuration
    Added commitlint configuration to enforce commit message standards.

---
+++ commitlint.config.js
@@ -0,0 +1,19 @@
+/** @type {import("@commitlint/types").UserConfig} */
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'deps', 'chore', 'docs', 'refactor', 'test', 'ci'],
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      ['web', 'web-e2e', 'eslint-config', 'shared', 'typescript-config'],
+    ],
+    'scope-empty': [0],
+    'subject-empty': [2, 'never'],
+    'subject-case': [2, 'always', 'lower-case'],
+  },
+};
